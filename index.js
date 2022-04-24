ad885929c35c6bdc32514560b9a71222c0f81dd8
Keyvan Alonso <keyvanalonso@gmail.com>
2022-04-23 21:25:31 -0400

    commit tree 0f42187a69fc251bd15c144ec250be310a6b227d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(nname, eemail) {
+    const pushData = {
+        name: nname,
+        email: eemail
+    };
+
+    const configObject = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify(pushData)
+    };
+
+    return fetch("http://localhost:3000/users", configObject)
+    .then((response) => response.json())
+    .then((object) => {
+        document.body.innerHTML = object['id']
+    })
+    .catch((error) => {
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData()
